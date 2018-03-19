77b9850dabc9efc75f59458f9b54356823453982
Jim Miller <millerjv@ge.com>
2018-03-19 09:56:56 -0400

    Port to Keras 2

--- step1_train_model/helper.py
+++ step1_train_model/helper.py
@@ -2,9 +2,7 @@
 
 import numpy as np
 
-from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D, Dropout
-from keras.layers import Convolution3D, MaxPooling3D, UpSampling3D
-from keras.layers import core
+from keras.layers import Input, Concatenate, Conv2D, MaxPooling2D, UpSampling2D, Dropout
 from keras.models import Model
 from keras.optimizers import Adam
 
@@ -64,47 +62,47 @@ def model5_MultiLayer(weights=False,
 	""" difference from model: img_rows and cols, order of axis, and concat_axis"""
 	
 	inputs = Input((img_rows, img_cols,n_cl_in))
-	conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(inputs)
-	conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv1)
+	conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+	conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
 	pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-	conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(pool1)
-	conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv2)
+	conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+	conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
 	pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-	conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(pool2)
-	conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv3)
+	conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+	conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
 	pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-	conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(pool3)
-	conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(conv4)
+	conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+	conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
 	pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
-	conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same')(pool4)
-	conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same')(conv5)
+	conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+	conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
 
-	up6 = merge([UpSampling2D(size=(2, 2))(conv5), conv4], mode='concat', concat_axis=3)
-	conv6 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(up6)
-	conv6 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(conv6)
+	up6 = Concatenate(axis=3)([UpSampling2D(size=(2, 2))(conv5), conv4])
+	conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+	conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
 
-	up7 = merge([UpSampling2D(size=(2, 2))(conv6), conv3], mode='concat', concat_axis=3)
-	conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(up7)
-	conv7 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv7)
+	up7 = Concatenate(axis=3)([UpSampling2D(size=(2, 2))(conv6), conv3])
+	conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
+	conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
 
-	up8 = merge([UpSampling2D(size=(2, 2))(conv7), conv2], mode='concat', concat_axis=3)
-	conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(up8)
+	up8 = Concatenate(axis=3)([UpSampling2D(size=(2, 2))(conv7), conv2])
+	conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
 	conv8 = Dropout(dropout)(conv8)
-	conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv8)
-	
-	up9 = merge([UpSampling2D(size=(2, 2))(conv8), conv1], mode='concat', concat_axis=3)
-	conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(up9)
+	conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+
+	up9 = Concatenate(axis=3)([UpSampling2D(size=(2, 2))(conv8), conv1])
+	conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
 	conv9 = Dropout(dropout)(conv9)
-	conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv9)
+	conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
 
-	conv10 = Convolution2D(n_cl_out, 1, 1, activation='sigmoid')(conv9)
-	model = Model(input=inputs, output=conv10)
+	conv10 = Conv2D(n_cl_out, (1, 1), activation='sigmoid')(conv9)
+	model = Model(inputs=inputs, outputs=conv10)
 
-	
+
 	model.compile(optimizer=Adam(lr=learning_rate),
 		loss=dice_coef_loss,
 		metrics=['accuracy'])
